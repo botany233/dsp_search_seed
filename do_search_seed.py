bbc6513fcf31dd69a47043fcb009950ec37962d9
botany233 <18201168383@163.com>
2025-10-16 17:51:09 +0800

    避免重名

--- do_search_seed.py
+++ do_search_seed.py
@@ -1,13 +1,13 @@
 from check_seed import check_seed
-from concurrent.futures import ProcessPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import cpu_count
-from threading import Thread
 
 # vein_names = "铁, 铜, 硅, 钛, 石, 煤, 油, 可燃冰, 金伯利, 分型硅, 有机晶体, 光栅石, 刺笋结晶, 单极磁石"
 # liquid = "水, 硫酸"
 # planet_types = ["地中海", "气态巨星", "冰巨星", "干旱荒漠", "灰烬冻土", "海洋丛林", "熔岩", "冰原冻土",
 #                 "贫瘠荒漠", "戈壁", "火山灰", "红石", "草原", "水世界", "黑石盐滩", "樱林海",
 #                 "飓风石林", "猩红冰湖", "热带草原", "橙晶荒漠", "极寒冻土", "潘多拉沼泽"]
+
 
 def check_batch(start_seed:int, end_seed:int, star_num: int, galaxy_condition:dict)->list[str]:
     result = []
@@ -42,7 +42,7 @@ if __name__ == "__main__":
 
     galaxy_condition = {"veins": {"单极磁石":4}, "stars":[star_condition_1, star_condition_2, star_condition_3]}
 
-    start_seed, end_seed = 0, 99999
+    start_seed, end_seed = 0, 19999
     start_star_num, end_star_num = 64, 64
     batch_size = 128
     max_thread = 20
@@ -57,9 +57,9 @@ if __name__ == "__main__":
                 f.writelines(map(lambda x: f"{x}\n", result))
     print(f"用时{perf_counter() - flag:.2f}s")
 
-    # flag = perf_counter()
-    # for seed in range(start_seed, end_seed+1):
-    #     if check_seed(seed, 64, galaxy_condition):
-    #         with open("result_single_thread.txt", "a") as f:
-    #             f.write(f"{seed}\n")
-    # print(f"用时{perf_counter() - flag:.2f}s")
+    flag = perf_counter()
+    for seed in range(start_seed, end_seed+1):
+        if check_seed(seed, 64, galaxy_condition):
+            with open("result_single_thread.txt", "a") as f:
+                f.write(f"{seed}\n")
+    print(f"用时{perf_counter() - flag:.2f}s")
