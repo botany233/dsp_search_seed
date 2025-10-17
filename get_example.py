11bd3035e2af033eaab6af91472c33fcfbbc93ce
botany233 <18201168383@163.com>
2025-10-17 12:26:34 +0800

    改了下example，方便查看c接口能获取哪些信息

--- get_example.py
+++ get_example.py
@@ -1,4 +1,10 @@
 from CApi import search_seed
 import json
 
-galaxy_data = json.loads(search_seed.single_search(233, 32))
+galaxy_json = search_seed.single_search(233, 32)
+
+with open("example.txt", "w", encoding="utf-8") as f:
+    f.write(galaxy_json)
+
+galaxy_data = json.loads(galaxy_json)
+print(galaxy_data)
