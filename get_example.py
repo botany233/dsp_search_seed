326dd3c608f3c6dd6ef274da3e9ac5d09e13823b
233dada <2474129326@qq.com>
2025-10-17 21:32:15 +0800

    修改示例输出为 `.json`

--- get_example.py
+++ get_example.py
@@ -7,13 +7,5 @@ print(type(galaxy_data))
 galaxy_json = json.dumps(galaxy_data, ensure_ascii=False, indent=4)
 print(galaxy_json)
 
-<<<<<<< Updated upstream
-with open("example.txt", "w", encoding="utf-8") as f:
-    f.write(galaxy_json)
-=======
 with open("example.json", "w", encoding="utf-8") as f:
     f.write(galaxy_json)
-
-galaxy_data = json.loads(galaxy_json)
-print(galaxy_data)
->>>>>>> Stashed changes
