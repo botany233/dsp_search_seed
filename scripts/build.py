57be8eb83d892aab8124f4302397b9057a38a665
233dada <2474129326@qq.com>
2025-10-25 16:45:57 +0800

    添加打包脚本

---
+++ scripts/build.py
@@ -0,0 +1,19 @@
+import PyInstaller.__main__
+import shutil
+import os
+
+
+PyInstaller.__main__.run(
+    [
+        "main.spec",
+        "--noconfirm",
+    ]
+)
+
+# 拷贝必要的文件到dist目录
+shutil.copy("README.md", os.path.join("dist", "戴森球计划种子搜索器", "README.md"))
+try:
+    shutil.copy("LICENSE", os.path.join("dist", "戴森球计划种子搜索器", "LICENSE"))
+except FileNotFoundError:
+    pass
+shutil.copytree("assets", os.path.join("dist", "戴森球计划种子搜索器", "assets"), dirs_exist_ok=True)
