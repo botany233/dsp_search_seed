94a8742f34f4d96f6dd373bff5083123e6ecf647
botany233 <18201168383@163.com>
2025-10-27 19:38:52 +0800

    种子查看器新建文件夹！

--- get_example.py
+++ get_example.py
@@ -1,7 +1,7 @@
 from CApi import search_seed
 import json
 
-galaxy_data = search_seed.get_galaxy_data_c(80116164, 64)
+galaxy_data = search_seed.get_galaxy_data_c(84954177, 64)
 # print(type(galaxy_data))
 
 galaxy_json = json.dumps(json.loads(galaxy_data), ensure_ascii=False, indent=4)
