ce990b29be7d83cc5b4e9644accbd08c52b4783a
botany233 <18201168383@163.com>
2025-10-28 16:48:53 +0800

    debug

--- GUI/seed_viewer/sort_seed/custom_sort.py
+++ GUI/seed_viewer/sort_seed/custom_sort.py
@@ -5,8 +5,12 @@ def handle_custom_sort(subtype: str) -> function:
         return get_seed_value
     elif subtype == "例子：恒星数量":
         return get_star_num_value
-    elif subtype == "自定义":
-        return get_custom_value
+    elif subtype == "自定义1":
+        return get_custom1_value
+    elif subtype == "自定义2":
+        return get_custom2_value
+    elif subtype == "自定义3":
+        return get_custom3_value
 
 #例子：使用种子号作为种子价值
 def get_seed_value(galaxy_data: GalaxyData) -> int:
@@ -19,5 +23,11 @@ def get_star_num_value(galaxy_data: GalaxyData) -> int:
 # 你可以完成下边的函数，通过自定义逻辑返回种子的价值
 # 返回类型需要支持使用str函数转化为字符串以及sorted函数排序，建议为float或int类型
 # GalaxyData具体包含哪些内容可以在CApi/search_seed.pyi中查看，也可以使用get_example.py获取对应的json格式
-def get_custom_value(galaxy_data: GalaxyData) -> float|int:
+def get_custom1_value(galaxy_data: GalaxyData) -> float|int:
+    return 0
+
+def get_custom2_value(galaxy_data: GalaxyData) -> float|int:
+    return 0
+
+def get_custom3_value(galaxy_data: GalaxyData) -> float|int:
     return 0
--- GUI/seed_viewer/sort_seed/sort_seed.py
+++ GUI/seed_viewer/sort_seed/sort_seed.py
@@ -1,10 +1,24 @@
-from .get_value_func import get_value_function
 from multiprocessing import cpu_count
 from concurrent.futures import ProcessPoolExecutor
 from CApi import get_galaxy_data_c
 
-def get_seed_value(args, value_func):
-    galaxy_data = get_galaxy_data_c(*args)
+from .custom_sort import handle_custom_sort
+from .planet_sort import handle_planet_sort
+from .star_sort import handle_star_sort
+from .veins_sort import handle_veins_sort
+
+def get_value_function(maintype:str, subtype: str) -> function:
+    if maintype == "矿物":
+        return handle_veins_sort(subtype)
+    elif maintype == "行星类别":
+        return handle_planet_sort(subtype)
+    elif maintype == "恒星类别":
+        return handle_star_sort(subtype)
+    elif maintype == "自定义":
+        return handle_custom_sort(subtype)
+
+def get_seed_value(seed_id, star_num, value_func):
+    galaxy_data = get_galaxy_data_c(seed_id, star_num)
     value = value_func(galaxy_data)
     return value
 
