4c1d4722a3b02402a9e4ce6586a39a7dd05d4851
botany233 <18201168383@163.com>
2025-10-31 21:24:52 +0800

    更新build相关名称

--- scripts/build.py
+++ scripts/build.py
@@ -11,9 +11,9 @@ PyInstaller.__main__.run(
 )
 
 # 拷贝必要的文件到dist目录
-shutil.copy("README.md", os.path.join("dist", "戴森球计划种子搜索器", "README.md"))
+shutil.copy("README.md", os.path.join("dist", "戴森球计划种子搜索查看器", "README.md"))
 try:
-    shutil.copy("LICENSE", os.path.join("dist", "戴森球计划种子搜索器", "LICENSE"))
+    shutil.copy("LICENSE", os.path.join("dist", "戴森球计划种子搜索查看器", "LICENSE"))
 except FileNotFoundError:
     pass
-shutil.copytree("assets", os.path.join("dist", "戴森球计划种子搜索器", "assets"), dirs_exist_ok=True)
+shutil.copytree("assets", os.path.join("dist", "戴森球计划种子搜索查看器", "assets"), dirs_exist_ok=True)
