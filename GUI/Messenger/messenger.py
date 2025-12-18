eb56f7f43fbe40bd2a205a49c3bc16070573ea0f
233dada <233dada@gmail.com>
2025-12-18 17:01:17 +0800

    注释更新

--- GUI/Messenger/messenger.py
+++ GUI/Messenger/messenger.py
@@ -10,10 +10,7 @@ class SearchSignal(QObject):
     Args:
         batch_id: int - 当前处理的批次ID
         total_batch: int - 总批次数量
-        total_seed_num: int - 当前批次找到的种子总数
-        last_seed: str - 最后处理的种子编号
-        start_time: float - 搜索开始时间戳
-        current_time: float - 当前时间戳
+        use_time: float - 搜索时间
     """
     searchEnd = Signal()
     new_find_seed = Signal(int, int, int)#total_seed_num, last_seed_id, last_star_num
